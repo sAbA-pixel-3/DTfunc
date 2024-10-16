f2cce04d148da13e2669c49fa9b52bfaf56cf03c
sAbA-pixel-3 <seconddonutbs@gmail.com>
2024-10-16 18:07:20 +0600

    Add files via upload

---
+++ DTfunc.py
@@ -0,0 +1,33 @@
+# методы и функции типов данных
+#       # 01234
+# name = 'Hello' # -1 -2 и тд идут в обратном направлении
+# print(name[::-1]) # покажет введенное слово в обратном направлении
+# [start:end:step]
+
+# print(name.upper()) # метод upper преобразует текст в верхний регистр
+# print(name.lower())
+# print(name.replace('Hello','jon')) # заменяет слово 
+# print(name.count('World')) # считает количество введенного слова
+# print(name.capitalize()) # первая буква с заглавной
+# print(name.title()) # делает каждое слово до конца текста с большой буквы, вне зависимости от . и ,
+# print(name.strip()) # убирает лишние пробелы в начале и конце кода
+# print(len(name)) # подсчет символов пробелы тоже считает
+
+# num = int(input("duvb: "))
+# print(max(34,24,1,4,23,5,num))
+# print(min(1,34,75,2,57,2353,4,num)) # выводит мин и макс введенное число, а так у int and float не имеют важных функций
+
+# q = 'Hello fucking bastards of fucking dump!'
+# print(q)
+# print(q.replace('fucking','lovly').replace('bastards','students').replace('dump','school'))
+# print("Number of digits are: ", len(q)) 
+
+
+
+
+
+
+
+
+
+
